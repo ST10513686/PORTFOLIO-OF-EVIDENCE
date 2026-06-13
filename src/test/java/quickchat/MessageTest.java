e2bde7092a6c0d89236cd946b05a64bdf69f3807
Ntati <Ntati@ntatiso>
2026-06-13 13:40:37 +0200

    Added Part 3 unit tests for arrays, longest message, search and delete

--- src/test/java/quickchat/MessageTest.java
+++ src/test/java/quickchat/MessageTest.java
@@ -84,7 +84,6 @@ public class MessageTest {
             Message m = new Message(ids[i], 0, RECIPIENT_1, texts[i]);
             assertEquals(expected[i], m.createMessageHash());
         }
-        
     }
 
     @Test
